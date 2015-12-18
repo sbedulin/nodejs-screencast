dea4c1c56482a43a2e6300184d66a86697eada8f
Sergey Bedulin <serge.bedulin@gmail.com>
2015-12-18 17:20:05 +0300

    15 - echo server

--- web-server/index.js
+++ web-server/index.js
@@ -1,18 +1,18 @@
 var http = require('http');
+var url = require('url');
 
-var server = new http.Server(); // http.Server -> net.Server -> EventEmitter
+var server = new http.Server(function (req, res) {
+    console.log( req.method, req.url );
 
-server.listen(1337, '127.0.0.1');
+    var urlParsed = url.parse(req.url, true);
 
-var counter = 0;
-server.on('request', function (req, res) {
-    res.end('Hello World! ' + ++counter);
+    if(urlParsed.pathname === '/echo' && urlParsed.query.message) {
+        res.setHeader('cache-control', 'no-cache, no-store, must-revalidate');
+        res.end( urlParsed.query.message );
+    } else {
+        res.statusCode = 404;
+        res.end('Page Not Found');
+    }
 });
 
-//console.log('server events: ', Object.keys(server._events).join(' | '));
-
-var emit = server.emit;
-server.emit = function (event) {
-    console.log('event fired: ', event);
-    emit.apply(server, arguments);
-};
+server.listen(1337, '127.0.0.1');
