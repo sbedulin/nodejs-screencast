8d0040ff18c3223d9935a0dff6cb3568b256d0d6
Sergey Bedulin <serge.bedulin@gmail.com>
2015-12-18 16:29:37 +0300

    13.4 - EventEmitter, memory leak fixed

--- ee-demo/leak.js
+++ ee-demo/leak.js
@@ -1,5 +1,6 @@
 var EventEmitter = require('events').EventEmitter;
 var db = new EventEmitter;
+db.setMaxListeners(0); // use with caution! don't forget to free up memory
 
 function Request() {
     var self = this;
@@ -10,13 +11,22 @@ function Request() {
         console.log(data);
     };
 
-    db.on('data', function (info) {
+    function onData(info) {
         self.send(info);
-    });
+    }
+
+    this.end = function () {
+        db.removeListener('data', onData);
+    };
+
+    db.on('data', onData);
 }
 
 setInterval(function () {
-    var request = new Request(); // GC can collect this obj
+    var request = new Request();
+    // ...
+    request.end();
+
     console.log(process.memoryUsage().heapUsed);
-    console.log(db._events.data.length);
+    //console.log(db);
 }, 200);
