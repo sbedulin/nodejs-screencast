de65941395f2b74ad75f9c6c92690217532c2cb8
Sergey Bedulin <serge.bedulin@gmail.com>
2016-01-04 12:40:03 +0300

    20.4 - sync + async with error handling

--- web-server/index.js
+++ web-server/index.js
@@ -2,11 +2,30 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function (req, res) {
-    if(req.url === '/') {
-        fs.readFile(__dirname + '/index.html', function (err, info) {
+    var index = __dirname + '/index.html';
+
+    if(req.url === '/async') {
+        fs.readFile(index, function (err, info) {
+            if(err) {
+                console.error(err);
+                res.statusCode = 500;
+                return res.send('Server error');
+            }
+
             res.end(info);
         });
-    } else if (req.url === '/now') {
+    } else if (req.url === '/sync') {
+        try {
+            return res.end(
+                fs.readFileSync(index)
+            );
+        } catch  (err) {
+            console.error(err);
+            res.statusCode = 500;
+            return res.send('Server error');
+        }
+
+    } else if (req.url === '/') {
         res.end(new Date().toString());
     }
 }).listen(3000);
