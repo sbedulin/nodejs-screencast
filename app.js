b14266f217051da6bf8813141772b9a8619936cc
Sergey Bedulin <serge.bedulin@gmail.com>
2016-01-06 14:15:54 +0300

    37 - user service, error handling

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
 var isDevelopment = app.get('env') === 'development';
+var HttpError = require('error').HttpError;
+var errorhandler = require('errorhandler');
 
 var logger = require('libs/log')(module);
 // optionally combine loggers
@@ -23,39 +25,35 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(require('middleware/sendHttpError'));
+
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new HttpError(404, 'Page Not Found');
+    err.status = 404;
+    next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (isDevelopment) {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    if (typeof err == 'number') { // next(404);
+        err = new HttpError(err);
+    }
+
+    if (err instanceof HttpError) {
+        res.sendHttpError(err);
+    } else {
+        if (app.get('env') === 'development') {
+            errorhandler()(err, req, res, next);
+        } else {
+            logger.error(err);
+            err = new HttpError(500);
+            res.sendHttpError(err);
+        }
+    }
 });
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,38 @@
+var User = require('models/user').User;
+var HttpError = require('error').HttpError;
+var ObjectID = require('mongodb').ObjectID;
+
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  User.find({}, function (err, users) {
+      if(err) return next(err);
+
+      res.json(users);
+  });
+});
+
+router.get('/:id', function(req, res, next) {
+    function userNotFound() {
+        return next(new HttpError(404, 'User Not Found'));
+    }
+
+    try {
+        var id = new ObjectID(req.params.id);
+    } catch(e) {
+        return userNotFound();
+    }
+
+    User.findById(id, function (err, user) {
+        if(err) return next(err);
+
+        if(!user) {
+            return userNotFound();
+        }
+
+        res.json(user);
+    });
 });
 
 module.exports = router;
