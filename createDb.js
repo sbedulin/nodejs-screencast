6c63ba5ab0d47ad7557449c8f187edc11182f0e6
Sergey Bedulin <serge.bedulin@gmail.com>
2016-01-06 03:11:13 +0300

    36.2 - refactoring and async.series

--- createDb.js
+++ createDb.js
@@ -6,33 +6,47 @@ var async = require('async');
 // 2. create & save 3 users
 // 3. close connection
 
-mongoose.connection.on('open', function () {
-    var db = mongoose.connection.db;
-    db.dropDatabase( function ( err ) {
-        if(err) throw err;
-
-        async.parallel([
-            function (cb) {
-                var vasya = new User({ username: 'Vasya', password: 'supervasya' });
-                vasya.save(function ( err ) {
-                    cb(err, vasya);
-                });
-            },
-            function (cb) {
-                var petya = new User({ username: 'Petya', password: '123' });
-                petya.save(function ( err ) {
-                    cb(err, petya);
-                });
-            },
-            function (cb) {
-                var admin = new User({ username: 'admin', password: 'thetruehero' });
-                admin.save(function ( err ) {
-                    cb(err, admin);
-                });
-            }
-        ], function (err, results) {
-            console.log(arguments);
-            mongoose.disconnect();
-        });
-    });
+async.series([
+    open,
+    dropDatabase,
+    createUsers,
+    close
+], function ( err, results ) {
+    console.log(arguments);
 });
+
+function open(callback) {
+    mongoose.connection.on('open', callback);
+}
+
+function dropDatabase(callback) {
+    var db = mongoose.connection.db;
+    db.dropDatabase(callback);
+}
+
+function createUsers(callback) {
+    async.parallel([
+        function (cb) {
+            var vasya = new User({ username: 'Vasya', password: 'supervasya' });
+            vasya.save(function ( err ) {
+                cb(err, vasya);
+            });
+        },
+        function (cb) {
+            var petya = new User({ username: 'Petya', password: '123' });
+            petya.save(function ( err ) {
+                cb(err, petya);
+            });
+        },
+        function (cb) {
+            var admin = new User({ username: 'admin', password: 'thetruehero' });
+            admin.save(function ( err ) {
+                cb(err, admin);
+            });
+        }
+    ], callback);
+}
+
+function close(callback) {
+    mongoose.disconnect(callback);
+}
